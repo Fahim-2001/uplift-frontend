5c8d1b8f196d64d2e7fe942ba87fbceb18bbf4ce
Fahim-2001 <mdyasirofficial2001@gmail.com>
2024-05-13 09:33:24 +0600

    U

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+import Logo from "../assets/images/main_logo.png"
 
 const Footer = () => {
   return (
-    <div className="mt-10">Footer</div>
+    <section className="mt-10 py-32 text-center grid justify-items-center">
+      <Link to="/"><img src={Logo} className="h-24" alt="" /></Link>
+      <p className="text-sm text-gray-700">Your partner to build yourself</p>
+    </section>
   )
 }
 
--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,11 +1,30 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import StyledButton from "../components/StyledButton";
+import { handlePurchase } from "../utils/programPurchase";
 
 const Program = () => {
   const programData = useLoaderData();
+  const navigate = useNavigate();
   const { program, instructor } = programData.data;
   const thingsToTeach = program?.thingsToTeach.split(",");
+  let user;
+  if (JSON.parse(localStorage.getItem("user"))) {
+    user = JSON.parse(localStorage.getItem("user"));
+  }
 
+  const handlePaymentProcess = async () => {
+    try {
+      if (user == null) {
+        navigate("/student-signup");
+        return;
+      }
+      const data = await handlePurchase(user, program);
+      localStorage.setItem("payment-info",JSON.stringify(data));
+      navigate("/payment-confirm")
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <section>
       {/* Banner, Title, Pricing */}
@@ -31,7 +50,7 @@ const Program = () => {
                 name: "Enroll Now",
                 style:
                   "bg-primary hover:bg-hover p-primary text-white rounded-md",
-                func: () => console.log("Clicked!"),
+                func: () => handlePaymentProcess(),
               }}
             />
           </div>
@@ -39,43 +58,58 @@ const Program = () => {
       </div>
 
       {/* Course Details and Things You Will Learn */}
-      <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="text-md">
-          <p className="text-lg font-medium underline mt-10 mb-2">
-            Course Details
-          </p>
-          <p className="mt-1">
-            Lead Instructor:{" "}
-            {instructor?.firstName + " " + instructor?.lastName}
-          </p>
-          <p className="mt-1">
-            Position: {instructor?.position + ", " + instructor?.workingCompany}
-          </p>
-          <p className="mt-1">Duration: {program?.duration} weeks</p>
-          <p className="mt-1">{program?.perWeekLiveClasses} classes per week</p>
-          <p className="mt-1">
-            Total enrolled: {program?.totalEnrolled} till now.
-          </p>
-          <p className="mt-1"></p>
-        </div>
-        <div>
-          <p className="text-lg font-medium underline mt-10 mb-2">
-            Things you will learn
-          </p>
-          <div className="text-md grid grid-cols-2">
-            {thingsToTeach?.map((thing, i) => (
-              <p key={i}>{thing}</p>
-            ))}
-          </div>
-        </div>
+      <div className="text-center text-md">
+        <p className="text-lg font-medium underline mt-10 mb-2">
+          Course Details
+        </p>
+        <p className="mt-1">
+          Lead Instructor: {instructor?.firstName + " " + instructor?.lastName}
+        </p>
+        <p className="mt-1">
+          Position: {instructor?.position + ", " + instructor?.workingCompany}
+        </p>
+        <p className="mt-1">Duration: {program?.duration} weeks</p>
+        <p className="mt-1">{program?.perWeekLiveClasses} classes per week</p>
+        <p className="mt-1">
+          Total enrolled: {program?.totalEnrolled} till now.
+        </p>
+        <p className="mt-1"></p>
       </div>
 
       {/* Registration deadline */}
-      <div className="my-10 bg-primary p-6 md:p-8 lg:p-10">
-        <p className="bg-base text-primary p-3 md:p-4 lg:p-5 text-xl md:text-2xl lg:text-3xl
-         text-center font-bold rounded-md">
+      <div className="my-10 bg-primary p-3 md:p-8 lg:p-10">
+        <p
+          className="bg-primary lg:bg-white text-base lg:text-primary p-3 md:p-4 lg:p-5 text-xl md:text-2xl lg:text-3xl
+         text-center font-bold rounded-md"
+        >
           Registration ends in 19th May, 2024
         </p>
+      </div>
+
+      {/* Timeline */}
+      <div>
+        <p className="text-lg font-medium underline mt-10 mb-2">
+          Things you will learn
+        </p>
+        <ol className="border-l border-neutral-300 dark:border-neutral-500">
+          {/* <!--First item--> */}
+          {thingsToTeach?.map((thing, i) => (
+            <li key={i}>
+              <div className="flex-start flex items-center pt-3">
+                <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
+                <p className="text-sm text-neutral-500 dark:text-neutral-300">
+                  01.07.2021
+                </p>
+              </div>
+              <div className="mb-6 ml-4 mt-2">
+                <h4 className="mb-1.5 text-xl font-semibold">{thing}</h4>
+                <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
+            </li>
+          ))}
+        </ol>
       </div>
     </section>
   );
